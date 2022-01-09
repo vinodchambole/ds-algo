ec04875b84b9ea5d14c3c7508807d0318974c739
vcham <vinodchambole@gmail.com>
2022-01-09 12:22:14 +0530

    Added solution 2 which has nlogn time complexity.

--- TwoNumberSum/Solution2.java
+++ TwoNumberSum/Solution2.java
@@ -1,6 +1,30 @@
 package TwoNumberSum;
 
+import java.util.Arrays;
+
 public class Solution2 {
 
+    public static void main(String[] args) {
+        int[] nums = {-21, 301, 12, 4, 65, 56, 210, 356, 9, -47};
+        int[] x = twoNumberSum(nums, 163);
+        System.out.println(Arrays.toString(x));
+    }
 
+    private static int[] twoNumberSum(int[] array, int targetSum) {
+        Arrays.sort(array);
+        int lowerIndex = 0;
+        int higherIndex= array.length-1;
+
+        while( lowerIndex < higherIndex){
+            if (array[lowerIndex] + array[higherIndex] == targetSum){
+                return new int[]{array[lowerIndex], array[higherIndex]};
+            }else if(array[lowerIndex] + array[higherIndex] < targetSum){
+                lowerIndex++;
+            }else {
+                higherIndex--;
+            }
+        }
+
+        return new int[0];
+    }
 }
--- TwoNumberSum/TwoNumberSum.md
+++ TwoNumberSum/TwoNumberSum.md
@@ -4,7 +4,8 @@ Data structures and algorithms
 ![img.png](img.png)
 
 
-# Hint
+# Hint 1
+// time complexity o(N) and space complexity o(N)
 
 Add the array elements in hashmap
 
@@ -15,3 +16,13 @@ y = targetSum -x;
 iterate the given array and check if y is present in hashmap.
 
 if it presents return x and y.
+
+# Hint 2
+// time complexity o(nlogn) (best array sorting)
+// space complexity o(1)
+
+sort the array
+
+use two indexes one from beginning and one from last and compare two numbers
+if sum is less than target increase lower index else higher index.
+
