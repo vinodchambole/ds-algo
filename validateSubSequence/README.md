a8ff455347815167ff3630764c4fffdb4bf7cb76
vcham <vinodchambole@gmail.com>
2022-01-09 14:32:49 +0530

    Added solution to validate sequence problem.

---
+++ validateSubSequence/README.md
@@ -0,0 +1,12 @@
+#validate subsequence 
+
+given array and sequence 
+
+compare elements of sequence to the array
+
+if any match found increase index of sequence and array both
+else increase index of array only.
+
+compare the size of sequence to the index at last.
+
+
---
+++ validateSubSequence/ValidateSubSequence.java
@@ -0,0 +1,28 @@
+package validateSubSequence;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Solution1 {
+
+    public static void main(String[] args) {
+
+        List<Integer> array = Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence = Arrays.asList(1, 6, -1, 10);
+        System.out.println(isValidSubsequence(array, sequence));
+    }
+
+    public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        // Write your code here.
+        int seqIndex = 0;
+        int arrayIndex = 0;
+
+        while (seqIndex < sequence.size() && arrayIndex < array.size()) {
+            if (sequence.get(seqIndex) == array.get(arrayIndex)) {
+                seqIndex++;
+            }
+            arrayIndex++;
+        }
+        return seqIndex == sequence.size();
+    }
+}
