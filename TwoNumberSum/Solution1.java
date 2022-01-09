bbc42515fcfa9b28f82c90247cc0e48387c749f0
vcham <vinodchambole@gmail.com>
2022-01-09 11:41:41 +0530

    improved solution1.

--- TwoNumberSum/Solution1.java
+++ TwoNumberSum/Solution1.java
@@ -1,10 +1,17 @@
 package TwoNumberSum;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 // time complexity o(N) and space complexity o(N)
 public class Solution1 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] x = twoNumberSum(nums, 7);
+        System.out.println(Arrays.toString(x));
+    }
     public static int[] twoNumberSum(int[] array, int targetSum) {
         Map<Integer, Integer> map = new HashMap();
         for (int i = 0; i < array.length; i++) {
@@ -12,7 +19,6 @@ public class Solution1 {
         }
         for (int i = 0; i < array.length; i++) {
             int temp = targetSum - array[i];
-
             if (map.containsKey(temp) && temp != array[i]) {
                 return new int[]{temp, array[i]};
             }
---
+++ TwoNumberSum/Solution2.java
@@ -0,0 +1,6 @@
+package TwoNumberSum;
+
+public class Solution2 {
+
+
+}
